dd92a061bb03e52a141828f828dbbe485339d2c2
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-12 14:19:23 -0600

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,12 +4,8 @@ int main()
  int a,b,sum = 0,rest = 0,mult = 0;
  printf("Ingrese dos números separadas por coma:\n");
  scanf("%d, %d, %d", &a, &b);
- do 
-   {
-   sum = a + b;
-   rest = a - b;
-   mult = a * b;
-   } 
+ sum = a + b;
+ rest = a - b;
+ mult = a * b;
  printf("%d, %d, %d", sum, rest, mult);
-return 0;
 }
