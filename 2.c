8c765f37df6f8f5d0c0c1c272c939c6ce19e6584
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-12 15:13:43 -0600

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-int r, circ, A;
+int r, circ = 0, A = 0;
 printf("Ingrese el radio:/n");
-scanf("%d", a);
+scanf("%d", r);
 circ = 2 * 3.14 * r;
 A = 3.14 * r * r;
 printf("%d, %d", circ, A);
