f50ff2afa110ff3fd93486b9bb9181d3f3dd0f5e
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-14 11:08:16 -0600

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main()
+float main()
 {
-int r, circ = 0, A = 0;
+float r, circ = 0, A = 0;
 printf("Ingrese el radio:/n");
-scanf("%d", r);
+scanf("%f", &r);
 circ = 2 * 3.14 * r;
 A = 3.14 * r * r;
-printf("%d, %d", circ, A);
+printf("%f, %f", circ, A);
 } 
