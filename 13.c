c350906984af393c4d98ad7dc65b298699bdeb4f
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 01:07:04 -0600

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int mat[N], N, i;
+int mat[N], N, i, s=0;
   printf("Ingrese el tamaño del arreglo unidimensional:\n");
 scanf("%d", &N);
   for (i=0;i<N;i++)
     {
-    printf("Ingrese el valor %d del arreglo:", i+1);
+    printf("Ingrese el valor entero %d del arreglo:", i+1);
     scanf("%d", &mat[i]);
-    
-    } 
+    s=s+mat[i];
+    }
+  printf("la suma de los elementos del arreglo es %d", s);
 } 
