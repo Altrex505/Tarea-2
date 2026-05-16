af7dc8b21339b9db02c64cee4b4022c651d672bb
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 20:54:51 -0600

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main()
 {
-
+float inv, tasa, total = 0;
+printf("Ingrese la cantidad de dinero a depositar:\n");
+scanf("%f", &inv);
+printf("Ingrese el porcentaje de la tasa de interés mensual del banco:\n"); 
+scanf("%f", &tasa);
+total = inv + inv * (tasa / 100);
+printf("El total a fin de mes será %f", total);
 } 
