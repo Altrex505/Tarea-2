d312d50106ae994b0a8d96292891cbd81dcf3deb
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 21:41:14 -0600

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+printf("Ingresa la distancia de ida en km y el tiempo de estancia en días en ese orden y sepeardos por coma:\n");
+scanf("%f, %f", &d, &t);
+if (d * 2 > 800) && (t > 7) 
+ {
+
+ }
+else
+ {
+
+ }
+  
+} 
