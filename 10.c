b7322f9275c979efaf056803d081bac0f4c56766
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 23:48:25 -0600

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 int main()
 {
+int i, N; pos= 0, neg = 0, nul = 0;
+for (i = 1; i <= 100; i++) 
+{
+  printf("Ingresa el numero %d: ", i);
+  scanf("%d", &N);
 
+  if (numero > 0) 
+  {
+  pos++;
+  } 
+  else 
+  {
+  if (numero < 0) 
+  {
+  neg++;
+  }
+  else 
+  {
+  nul++;
+  }
+  } 
+    printf("Numeros positivos: %d\n", pos);
+    printf("Numeros negativos: %d\n", neg);
+    printf("Numeros nulos (ceros): %d\n", nul);
 } 
