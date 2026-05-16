989c117430fbc1f5dab7fc92ac30707aacd14a7d
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 17:10:53 -0600

    Update 16.c

--- 16.c
+++ 16.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main()
 {
-int N, i;
-  for (i=0;i<N;i++)
-    {
-    
-    }
+int i, N=2;
+int mat[100];
+for (i=0;i<100;i++)
+  {
+    mat[i]=N;
+    N=N+2;
+  }
+for (i=0;i<100;i++)
+  {
+    printf("%d\n", mat[i]);
+  }
 }
