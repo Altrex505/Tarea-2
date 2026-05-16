86e4c0fe5dd78d672756c8fa87c439f40aab09b7
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 21:14:34 -0600

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,5 +2,15 @@
 int main()
 {
 float m, total;
-if (m > 2500
+printf("Ingresa el monto:\n");
+scanf("%f", &m);
+if (m > 2500) 
+{
+  total = m - m * 0.08;
+} 
+else
+{
+  total = m;
+} 
+printf("el total a pagar es %f", total); 
 } 
