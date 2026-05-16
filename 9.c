ba7569242342dae3b702de37cc2a9a276d99f7ed
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 23:15:50 -0600

    Update 9.c

--- 9.c
+++ 9.c
@@ -2,7 +2,7 @@
 int main()
 {
 int N,N1,N2,N3,N4,N5,N6,N7,N8,N9,N10;
-printf("Ingrese un número entero mayor o igual a 1");
+printf("Ingrese un número entero mayor o igual a 1:\n");
 scanf("%d", &N);
 do
   {
@@ -22,7 +22,7 @@ printf("%d \n %d \n %d \n %d \n %d \n %d \n %d \n %d \n %d \n %d", N1, N2, N3, N
 }
 else
 {
-printf("Número no válido, ingrese otro");
+printf("Número no válido, ingrese otro:\n");
 scanf("%d", &N);
 }
   } 
