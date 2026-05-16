b3eed814d47f27ea830e3637540cd6f523c04ced
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 22:12:55 -0600

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main()
 {
-float
+int a, b, c;
+printf("Ingrese dos número enteros separados por espacio:\n");
+scanf("%d %d", &a &b);
+c = a / b;
+if (c%2);
+{
+printf("");
+}
+else
+{
+printf("");
+}
+  
 } 
