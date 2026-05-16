bda5f882f369e461aa0161d6edfa71c398305f2c
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 00:24:29 -0600

    Update 11.c

--- 11.c
+++ 11.c
@@ -2,9 +2,12 @@
 int main()
 {
   int s=0, i;
-for (i=12,i<50,i++++) 
+for (i=12;i<50;i++) 
+  {
+  if (i % 2 == 0) 
   {
   s=s+i;
-} 
+  } 
+  } 
   printf("la suma de pares entre el 10 y 50 es %d", s);
 } 
