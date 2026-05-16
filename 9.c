129bc4b2bad415d038f09b4b5f4827bd692d7a26
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 23:14:25 -0600

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 int main()
 {
-  
+int N,N1,N2,N3,N4,N5,N6,N7,N8,N9,N10;
+printf("Ingrese un número entero mayor o igual a 1");
+scanf("%d", &N);
+do
+  {
+if (N >= 1)
+{
+  N1 = N * 1;
+  N2 = N * 2;
+  N3 = N * 3;
+  N4 = N * 4;
+  N5 = N * 5;
+  N6 = N * 6;
+  N7 = N * 7;
+  N8 = N * 8;
+  N9 = N * 9;
+  N10 = N * 10;
+printf("%d \n %d \n %d \n %d \n %d \n %d \n %d \n %d \n %d \n %d", N1, N2, N3, N4, N5, N6, N7, N8, N9, N10);
+}
+else
+{
+printf("Número no válido, ingrese otro");
+scanf("%d", &N);
+}
+  } 
+while (N < 1);
 } 
