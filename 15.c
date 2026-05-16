7594d9e2a04bdc12b6821d3519f8fe95299f817f
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 15:49:08 -0600

    Update 15.c

--- 15.c
+++ 15.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main()
 {
-
+int N, i;
+  printf("Ingrese el tamaño del arreglo unidimensional:\n");
+scanf("%d", &N);
+  int mat[N];
+  for (i=0;i<N;i++)
+    {
+    printf("Ingrese el valor entero %d del arreglo:", i+1);
+    scanf("%d", &mat[i]);
+    }
+  for (i=0;i<N;i++) 
+    {
+    
+    } 
+  printf("
 } 
