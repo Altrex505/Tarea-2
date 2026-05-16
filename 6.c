d5a93a1c8f032ebdbb99dfbde6c91ad9234f20f2
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-15 21:24:48 -0600

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main()
 {
-
+float s, sa;
+printf("Ingrese el sueldo:\n");
+scanf("%f", s);
+if(s < 1000)
+{
+  sa = s * 1.15;
+} 
+else
+{
+  sa = s * 1.12;
+} 
+printf("el sueldo con aumento es %f", sa);
 } 
