6a0c8e1c9b4f243b8c931d432a7f3ac1984ee1b3
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 15:56:19 -0600

    Update 15.c

--- 15.c
+++ 15.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-int N, i;
+int N, i, pos, neg, nul;
   printf("Ingrese el tamaño del arreglo unidimensional:\n");
 scanf("%d", &N);
   int mat[N];
@@ -12,7 +12,20 @@ scanf("%d", &N);
     }
   for (i=0;i<N;i++) 
     {
-    
+    if (mat[i] > 0) 
+  {
+  pos++;
+  } 
+  else 
+    {
+  if (mat[i] < 0) 
+  {
+  neg++;
+  }
+  else 
+  {
+  nul++;
+  }
     } 
-  printf("
+  printf("El arreglo tiene %d positivos, %d negativos y %d nulos (iguales a cero).", pos, neg, nul);
 } 
