f8c7e67ff4fd9c5fc4f7434540b8360e3710f1d3
Altrex505 <diegodanielnoguezlopez@gmail.com>
2026-05-16 00:43:34 -0600

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main()
 {
-int N, i, p;
+int N, i, p=1;
 printf("Ingrese el número de naturales: \n");
-scanf("%d", & N);
-i=N;
-for(i
+scanf("%d", &N);
+if(N>=1)
+{
+for(i=2;i<=N;i++)
   {
-
+p=p*i
   } 
+}
+  else
+{
+p=0
+}
+  printf("El producto de los primeros %d naturales es %d.",N,p);
 } 
